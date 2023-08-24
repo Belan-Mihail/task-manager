f5c151df1828a2836ac0ccb4d50b7512253303d7
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-08-24 14:56:35 +0000

    Commit

---
+++ taskmanager/routes.py
@@ -0,0 +1,12 @@
+# We're going to start using some Flask functionality, so we can import render_template from flask to start with.
+# Then, from our main taskmanager package, let's import both 'app' and 'db'.
+
+from flask import render_template
+from taskmanager import app, db
+
+# For simplicity to get the app running, we'll create a basic app route using the root-level directory of slash.
+# This will be used to target a function called 'home', which will just return the rendered_template
+# of "base.html" that we will create shortly.
+@app.route("/")
+def home():
+    return render_template("base.html")
